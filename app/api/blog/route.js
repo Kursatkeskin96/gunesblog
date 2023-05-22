8cb4a6beadb793d3ba9a5807bb6910f815cc1843
Kursat Keskin <kursatkeskinn@gmail.com>
2023-05-22 19:52:24 +0100

    blog api changes

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,14 +1,26 @@
-import Blog from '../../../models/blog'
-import { connectToDB } from '../../../utils/database'
+import Blog from '../../../models/blog';
+import { connectToDB } from '../../../utils/database';
 
 export const GET = async (request) => {
-    try {
-        await connectToDB()
+  try {
+    await connectToDB();
 
-        const blogs = await Blog.find({}).populate('creator')
+    const timestamp = Date.now(); // Generate a unique timestamp
+    const blogs = await Blog.find({}).populate('creator');
 
-        return new Response(JSON.stringify(blogs), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch all blogs", { status: 500 })
-    }
-}
+    const response = new Response(JSON.stringify(blogs), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store', // Disable caching
+      },
+    });
+
+    response.url += `?timestamp=${timestamp}`; // Append cache-busting query parameter
+
+    return response;
+  } catch (error) {
+    const response = new Response('Failed to fetch all blogs', { status: 500 });
+    return response;
+  }
+};
