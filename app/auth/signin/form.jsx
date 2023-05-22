e7d6e825ba8bafceda27f88b23921d90671db218
Kursat Keskin <kursatkeskinn@gmail.com>
2023-05-22 19:20:45 +0100

    form turn back

--- app/auth/signin/form.jsx
+++ app/auth/signin/form.jsx
@@ -8,35 +8,50 @@ import gunes3 from '@/assets/guneslogin.jpg'
 
 export const LoginForm = () => {
   const router = useRouter();
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false);
+  const [formValues, setFormValues] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
 
-    if (password === '' || username === '') {
-        console.log("Fill all fields!")
-        return
-    }
-
-    if (password.length < 6) {
-        console.log("Password must be at least 6 characters long")
-        return
-    }
-
+  const onSubmit = async (e) => {
+    e.preventDefault();
     try {
-        const res = await signIn('credentials', { username, password, redirect: false })
+      setLoading(true);
+      setFormValues({ username: "", password: "" });
 
-        if (res?.error == null) {
-            router.push("/")
-        } else {
-            console.log("Error occured while logging")
-        }
+      const res = await signIn("credentials", {
+        redirect: false,
+        username: formValues.username,
+        password: formValues.password,
+        callbackUrl,
+      });
+
+      setLoading(false);
+
+      console.log(res);
+      if (!res?.error) {
+        router.push(callbackUrl);
+      } else {
+        setError("Hatali kullanici adi veya sifre");
+      }
     } catch (error) {
-        console.log(error)
+      setLoading(false);
+      setError(error);
     }
-}
+  };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const input_style =
+    "form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none";
 
   return (  
     <div className="grid lg:grid-cols-2 sm:grid-cols-1 max-w-6xl">
@@ -45,22 +60,25 @@ export const LoginForm = () => {
         src={gunes3}
         width={300}
         height={300}
-        className="mx-auto rounded-md "
+        className="mx-auto rounded-md"
         alt="Picture of the author"
         priority={true}
       />
     </div>
     <div className="">
-      <form onSubmit={handleSubmit} className="h-[400px] lg:w-[400px] shadow-lg p-10">
+      <form onSubmit={onSubmit} className="h-[400px] lg:w-[400px] shadow-lg p-10">
+        {error && (
+          <p className="text-center bg-red-300 py-4 mb-6 rounded">{error}</p>
+        )}
         <div className="mb-6">
           <label htmlFor="username">Kullanıcı Adı</label>
           <input
             required
             type="text"
             name="username"
-            onChange={(e) => setUsername(e.target.value)} 
+            value={formValues.username}
+            onChange={handleChange}
             placeholder="Kullanıcı adı"
-            className= "form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
        
           />
         </div>
@@ -70,16 +88,19 @@ export const LoginForm = () => {
             required
             type="password"
             name="password"
-            onChange={(e) => setPassword(e.target.value)} 
+            value={formValues.password}
+            onChange={handleChange}
             placeholder="********"
-            className= "form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
     
           />
         </div>
         <button
           type="submit"
-          className="bg-[#fdb44b] inline-block px-7 mt-5 py-4 text-white font-medium text-sm leading-snug uppercase rounded shadow-md active:shadow-lg transition duration-150 ease-in-out w-full"
+          style={{ backgroundColor: loading ? "#ccc" : "#fdb44b" }}
+          className="inline-block px-7 mt-5 py-4 text-white font-medium text-sm leading-snug uppercase rounded shadow-md active:shadow-lg transition duration-150 ease-in-out w-full"
+          disabled={loading}
         >
+          {loading ? "Giriş Yapılıyor..." : "Giriş Yap"}
         </button>
       </form>
     </div>
