f9404e57f494a5f8aa2f122506e9afb5c1cec039
Kursat Keskin <kursatkeskinn@gmail.com>
2023-05-22 19:44:39 +0100

    blog api change

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -5,7 +5,7 @@ export const GET = async (request) => {
     try {
         await connectToDB()
 
-        const blogs = await Blog.find({}).populate('')
+        const blogs = await Blog.find({}).populate('creator')
 
         return new Response(JSON.stringify(blogs), { status: 200 })
     } catch (error) {
